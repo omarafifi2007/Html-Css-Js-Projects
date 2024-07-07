6239cfd24bdee887b862fb2d53501d143d25329e
Omar Afifi <138309729+omarafifi2007@users.noreply.github.com>
2024-07-07 03:16:46 +0300

    Flappy Bird

---
+++ Flappy Bird/main.js
@@ -0,0 +1,45 @@
+const hole = document.getElementById('hole');
+const obstacle = document.getElementById('obstacle');
+const bird = document.getElementById('bird');
+let score = 0;
+
+hole.addEventListener('animationiteration', () => {
+    let ra = Math.random() * (500 - 150);
+    hole.style.top = ra + 'px';
+    score++;
+});
+
+setInterval(function () {
+    let birdTop = parseInt(getComputedStyle(bird).getPropertyValue('top'));
+    if (!isJumping) {
+        bird.style.top = birdTop + 3 + 'px';
+    }
+    let obstacleLeft = parseInt(getComputedStyle(obstacle).getPropertyValue('left'));
+    let holeTop = parseInt(getComputedStyle(hole).getPropertyValue('top'));
+    if (birdTop > 480 || (obstacleLeft < 20 && (birdTop > holeTop + 150 || birdTop < holeTop))) {
+        alert(`Game Over Your Score ${score}`);
+        bird.style.top = 100 + 'px';
+        obstacle.style.left = '100%';
+        hole.style.left = '100%';
+        score = 0;
+    }
+}, 10);
+
+let isJumping = false;
+
+document.addEventListener('keyup', () => {
+    isJumping = true;
+    let jumpTimer = 0;
+    let jumpInterval = setInterval(function () {
+        jumpTimer++;
+        let birdTop = parseInt(getComputedStyle(bird).getPropertyValue('top'));
+        if (birdTop > 0 && jumpTimer < 15) {
+            bird.style.top = birdTop - 5 + 'px';
+        }
+        if (jumpTimer > 20) {
+            clearInterval(jumpInterval);
+            isJumping = false;
+            jumpTimer = 0;
+        }
+    })
+})
